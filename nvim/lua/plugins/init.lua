3d8b8b40efd2ccc75ba40109f93a48c6cd03cf24
Tomasz <tomy.brak@gmail.com>
2025-11-25 02:46:12 +0100

    markdown render

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -13,6 +13,16 @@ return {
     end,
   },
 
+  {
+    "MeanderingProgrammer/render-markdown.nvim",
+    dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-mini/mini.nvim" }, -- if you use the mini.nvim suite
+    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-mini/mini.icons' },        -- if you use standalone mini plugins
+    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
+    ---@module 'render-markdown'
+    ---@type render.md.UserConfig
+    opts = {},
+    lazy=false
+  },
   -- test new blink
   -- { import = "nvchad.blink.lazyspec" },
 
