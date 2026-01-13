96c2192154e4d9b46995017cfe1a869eb59b3b44
Tomasz Brak <tomy.brak@gmail.com>
2026-01-13 20:33:43 +0100

    lsp action nvim dap

--- nvim/lua/plugins/snacks.lua
+++ nvim/lua/plugins/snacks.lua
@@ -7,7 +7,7 @@ return {
   opts = {
     bigfile = { enabled = true },
     dashboard = { enabled = true },
-    explorer = { enabled = true },
+    explorer = { enabled = true,   auto_close = true,},
     indent = { enabled = true },
     input = { enabled = true },
     notifier = {
---
+++ nvim/lua/plugins/stepforvimkind.lua
@@ -0,0 +1,21 @@
+return {
+  "mfussenegger/nvim-dap",
+  dependencies = {
+    "jbyuki/one-small-step-for-vimkind",
+  },
+  lazy = false,
+  config = function()
+    local dap = require "dap"
+    dap.configurations.lua = {
+      {
+        type = "nlua",
+        request = "attach",
+        name = "Attach to running Neovim instance",
+      },
+    }
+
+    dap.adapters.nlua = function(callback, config)
+      callback { type = "server", host = config.host or "127.0.0.1", port = config.port or 8086 }
+    end
+  end,
+}
